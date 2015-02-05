c415c719b7a8146a08b0c30cab5c890da2c64b70
Staffan Olsson <staffan@repos.se>
2015-02-05 14:43:15 +0100

    Defines addAfter and addFirst, essential methods for Author.

--- backbone/CollectionImpl.js
+++ backbone/CollectionImpl.js
@@ -1,3 +1,4 @@
+'use strict';
 
 // The "Turn bare objects into model references" part for Backbone's annotated source is interesting
 // http://backbonejs.org/docs/backbone.html#section-90
@@ -5,3 +6,21 @@
 var Backbone = require('backbone');
 
 var Collection = module.exports = Backbone.Collection;
+
+Collection.prototype.addAfter = function addAfter(newModel, referenceModel) {
+  var ix = this.indexOf(referenceModel);
+  if (referenceModel < 0) {
+    throw 'Reference model not found in collection';
+  }
+  if (this.contains(newModel)) {
+    throw 'Already a collection member';
+  }
+  this.add(newModel, {at:ix});
+};
+
+Collection.prototype.addFirst = function addFirst(newModel) {
+  if (this.contains(newModel)) {
+    throw 'Already a collection member';
+  }
+  this.add(newModel, {at:0});
+};
---
+++ test/CollectionSortSpec.js
@@ -0,0 +1,16 @@
+
+describe("Collection", function() {
+
+  xdescribe("Forbids the classic Backbone .sort because persistence should not be affected by presentation sorting", function() {
+
+  });
+
+  xdescribe("A way to disconnect from the underlying order so a regular backbone .sort() can run safely", function() {
+
+  });
+
+  xdescribe("How this makes ordered insertion throw unsupported messages", function() {
+
+  });
+
+});
