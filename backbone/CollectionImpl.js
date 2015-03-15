23618cc94180b744ada35958368893cac2177f9f
Staffan Olsson <staffan@repos.se>
2015-03-15 13:37:22 +0100

    Does the API split without code duplication

--- backbone/CollectionImpl.js
+++ backbone/CollectionImpl.js
@@ -1,12 +1,9 @@
 'use strict';
 
-// The "Turn bare objects into model references" part for Backbone's annotated source is interesting
-// http://backbonejs.org/docs/backbone.html#section-90
-
 var _ = require('underscore');
-var Backbone = require('backbone');
+var CollectionB = require('./CollectionBImpl');
 
-var Collection = module.exports = Backbone.Collection.extend({
+var Collection = module.exports = CollectionB.extend({
 
   set: function set(models) {
     var singular = !_.isArray(models);
@@ -16,52 +13,7 @@ var Collection = module.exports = Backbone.Collection.extend({
         throw new Error('Attributes property required, transfers state');
       }
     }
-    return Backbone.Collection.prototype.set.apply(this, arguments);
+    return CollectionB.prototype.set.apply(this, arguments);
   }
 
 });
-
-// We actually modify Backbone's prototype with the below, but that's our own Backbone supposedly
-
-Collection.prototype.addAfter = function addAfter(newModel, referenceModel) {
-  var ix = this.indexOf(referenceModel);
-  if (referenceModel < 0) {
-    throw 'Reference model not found in collection';
-  }
-  if (this.contains(newModel)) {
-    throw 'Already a collection member';
-  }
-  this.add(newModel, {at:ix});
-};
-
-Collection.prototype.addFirst = function addFirst(newModel) {
-  if (this.contains(newModel)) {
-    throw 'Already a collection member';
-  }
-  this.add(newModel, {at:0});
-};
-
-Collection.prototype.subset = function(options) {
-  if (!options.filter) {
-    throw 'Subset options must have a filter property';
-  }
-  var subset;
-  if (options.filter.where) {
-    subset = this.subsetConnect(this.where(options.filter.where));
-  }
-  if (typeof subset == 'undefined') {
-    console.log('Unrecognized filter', options);
-    throw 'Unrecognized filter';
-  }
-  if (options.immerse) {
-    subset.on('add', options.immerse);
-  }
-  subset.on('add', this.add.bind(this));
-  subset.on('remove', this.remove.bind(this));
-  return subset;
-};
-
-Collection.prototype.subsetConnect = function(models) {
-  var subset = new Collection(models);
-  return subset;
-};
